425b653e5115ea422cd9ec6b7892a2d259a7d835
Mikko Tervala <miksu103@gmail.com>
2025-02-17 21:22:48 +0100

    Add information for panelization

---
+++ pcb/panelize_script.ps1
@@ -0,0 +1,9 @@
+kikit panelize `
+    --layout 'grid; rows: 2; cols: 2; space: 2mm; rotation: 90deg' `
+    --tabs annotation `
+    --cuts 'mousebites; drill: 0.5mm; spacing: 1mm; offset: 0.2mm; prolong: 0.5mm' `
+    --framing 'frame; width: 5mm; space: 2mm; cuts: v' `
+    --tooling '4hole; hoffset: 2.5mm; voffset: 2.5mm; size: 2mm' `
+    --fiducials '4fid; hoffset: 12mm; voffset: 5mm; coppersize: 1mm; opening: 2mm;' `
+    --post 'millradius: 1mm' `
+    C:\Users\miksu\Documents\GitHub\lighthouse\pcb\lighthouse-pcb.kicad_pcb C:\Users\miksu\Documents\GitHub\lighthouse\pcb\panelize\panelized-lighthouse-pcb.kicad_pcb
